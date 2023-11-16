69d9a6e968e9e16660453a9114d33962665ad9c0
Marziap <marz.pirozzi@studenti.unina.it>
2023-11-16 15:57:31 +0100

    Initial Commit

---
+++ MyZoom/MyZoomApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyZoomApp.swift
+//  MyZoom
+//
+//  Created by Marzia Pirozzi on 16/11/23.
+//
+
+import SwiftUI
+
+@main
+struct MyZoomApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
